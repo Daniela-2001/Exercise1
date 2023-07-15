c01ff6af34ec84c9b5fe2e50201da9d089be658b
Daniela <Daniela@DESKTOP-P4BHM2A>
2023-07-15 12:50:51 +0100

    Adicionar arquivos de projeto.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise1
+
+     // Write a program that takes two numbers and displays the sum result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1=5;
+            int num2=10;
+            int soma = num1 + num2;
+
+            Console.WriteLine("O resultado da some é " + soma);
+            Console.ReadLine();
+        }
+    }
+}
